1b0b15ee481f1b974b47a50a58f91b318d2cc920
toran billups <toranb@gmail.com>
2018-07-01 09:46:39 -0500

    updated a test to try testem

--- tests/integration/components/child-component-test.js
+++ tests/integration/components/child-component-test.js
@@ -6,19 +6,9 @@ moduleForComponent('child-component', 'Integration | Component | child component
 });
 
 test('it renders', function(assert) {
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
+  this.set('item', '1');
+  this.set('add', () => {});
+  this.render(hbs`{{child-component item=item click=add}}`);
 
-  this.render(hbs`{{child-component}}`);
-
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
-  this.render(hbs`
-    {{#child-component}}
-      template block text
-    {{/child-component}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
+  assert.equal(this.$().text().trim(), '1 add');
 });
